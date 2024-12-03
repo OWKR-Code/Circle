16c101f1d97144c114f9ae4262423db3435a900d
xam2008 <104933360+xam2008@users.noreply.github.com>
2024-12-04 00:33:44 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, initial-scale=1">
+             <title>Document</title>
+             <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+             <link rel="stylesheet" href="CSS/iconsConfig.css" >
+             <link rel="stylesheet" href="CSS/header.css >
+</head>
+<body>
+<img class="DEFAULT_ICON_HOME" src="images/home.svg" alt="" srcset="">
+</body>
+</html>
