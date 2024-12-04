db1ada0d90c801f271c328c33a440d9858d7ef8e
xam2008 <104933360+xam2008@users.noreply.github.com>
2024-12-04 10:40:59 +0100

    Header replaced icons with text, still working on CSS for header

--- include/header.php
+++ include/header.php
@@ -3,9 +3,12 @@
 <head>
 <meta charset="UTF-8">
              <meta name="viewport" content="width=device-width, initial-scale=1">
-            
+            <link rel="stylesheet" href="CSS/iconsConfig.css">
+            <link rel="stylesheet" href="header.css">
 </head>
 <body>
-  
+  <div class="header_sec1_back_gray">
+<p class="HeaderItem_text">Home</p>
+  </div>
 </body>
 </html>
