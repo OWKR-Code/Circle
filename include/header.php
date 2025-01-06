116194959e8edc3b5c8ea2a92eb5b51d9a14a6c5
xam2008 <104933360+xam2008@users.noreply.github.com>
2025-01-06 22:36:32 +0100

    Complete new start

--- include/header.php
+++ include/header.php
@@ -3,12 +3,11 @@
 <head>
 <meta charset="UTF-8">
              <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="CSS/iconsConfig.css">
-            <link rel="stylesheet" href="header.css">
+            <link rel="stylesheet" href="CSS/header.css">
 </head>
 <body>
-  <div class="header_sec1_back_gray">
-<p class="HeaderItem_text">Home</p>
+<p>Home</p>
+
   </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <meta charset="UTF-8">
              <meta name="viewport" content="width=device-width, initial-scale=1">
              <title>Document</title>
-                    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
-             <link rel="stylesheet" href="CSS/iconsConfig.css" >
-             <link rel="stylesheet" href="CSS/header.css">
+             <link rel="stylesheet" href="CSS/main.css" >
 </head>
 <body>
-<?php include 'include/header.php'?>
+  <header>
+  <?php include "include/header.php"; ?>
+  </header>
 </body>
 </html>
